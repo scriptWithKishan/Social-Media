795e843d86c5d64af07a865db5c40397ab0edd5f
= <kishan.2785.2003@gmail.com>
2024-08-30 15:59:35 +0530

    login and register page

---
+++ backend-project/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const dbPath = path.join(__dirname, "project.db");
+let db = null;
+
+// Use CORS middleware
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with your frontend URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(8000, () => {
+      console.log("Server Running at http://localhost:8000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+initializeDbAndServer();
+
+app.post("/users/register", async (request, response) => {
+  const { id, username, email, password } = request.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const selectUserQuery = `SELECT * FROM Users WHERE username = '${username}'`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    const createUserQuery = `
+            INSERT INTO 
+                Users (id, username, email, password)
+            VALUES (
+                '${id}',
+                '${username}',
+                '${email}',
+                '${hashedPassword}'
+            );
+        `;
+    const dbResponse = await db.run(createUserQuery);
+    console.log(dbResponse);
+    const newUserId = dbResponse.lastID;
+    response.send(`Created New User Successfully`);
+  } else {
+    response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/users/login", async (request, response) => {
+  const { username, password } = request.body;
+  const selectUserQuery = `SELECT * FROM Users WHERE username = '${username}';`;
+  const dbUser = await db.get(selectUserQuery);
+
+  if (dbUser === undefined) {
+    response.status(400);
+    response.send("Invalid User");
+  } else {
+    const isPasswordMatched = await bcrypt.compare(password, dbUser.password);
+    if (isPasswordMatched === true) {
+      const payload = {
+        username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid Password");
+    }
+  }
+});
